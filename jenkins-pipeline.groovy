df13bf090ee142dff44595b076530d4a8852d440
Gerrit Dongus <gdongus@gmail.com>
2023-12-21 22:23:02 +0100

    Add multi repos example code in Jenkinsfile

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                // Your build steps here
+            // Your build steps here
             }
         }
     }
@@ -13,15 +13,17 @@ pipeline {
                 // Determine build state based on build result
                 def state = (currentBuild.result == 'SUCCESS') ? 'SUCCESS' : 'FAILURE'
                 def message = (currentBuild.result == 'SUCCESS') ? 'Build succeeded' : 'Build failed'
-
-                // Update GitHub commit status
-                step([$class: "GitHubCommitStatusSetter", 
-                      reposSource: [$class: "ManuallyEnteredRepositorySource", url: "http://github.com/username/repo"], 
-                      contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins"], 
-                      statusResultSource: [$class: "ConditionalStatusResultSource", 
-                                           results: [[$class: "AnyBuildResult", 
-                                                      state: state, 
+                repos = ['repo1', 'repo2', 'repo3']
+                repos.each { repo ->
+                    // Update GitHub commit status
+                    step([$class: 'GitHubCommitStatusSetter',
+                      reposSource: [$class: 'ManuallyEnteredRepositorySource', url: repo],
+                      contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: 'ci/jenkins'],
+                      statusResultSource: [$class: 'ConditionalStatusResultSource',
+                                           results: [[$class: 'AnyBuildResult',
+                                                      state: state,
                                                       message: message]]]])
+                }
             }
         }
     }
